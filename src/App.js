d65661cbf3668cce271b7dde051bdc9a98d015ef
laiqbudye <laiqbudye@gmail.com>
2020-04-25 18:19:23 +0530

    Structure || added basic structure of a project ( added components structure )

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
+import Cards from './components/Cards/Cards';
+import Chart from './components/Chart/Chart';
+import CountryPicker from './components/CountryPicker/CountryPicker';
 
 class App extends React.Component {
-  render() {  
-    return(
-    <div>
-      <h1>Hello World</h1>
-    </div>
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <Cards />
+        <CountryPicker />
+        <Chart />
+        </div>
     )
   };
 }
